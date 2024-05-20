2c62b5acf97e24ddf58fdc3aacbdf0bab2a09c39
Abhinav Gupta <108685322+HypertextCoder@users.noreply.github.com>
2024-05-20 09:22:18 +0530

    Update Program_VII_1_SeriesA.java

--- Program_VII_1_SeriesA.java
+++ Program_VII_1_SeriesA.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Program_VII_1_SeriesA {
+public class Program_VII_2_SeriesA {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
